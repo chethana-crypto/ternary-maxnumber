ef71f93ab26e4eb4343fdbac1f3b3531c4ab56f3
chethana-crypto <69971162+chethana-crypto@users.noreply.github.com>
2021-09-12 16:20:21 +0530

    ternary-operator
    a program to find  largest number using ternary operator

---
+++ ternaryoperator/src/ternaryoperator/ternaryoperator.java
@@ -0,0 +1,10 @@
+package ternaryoperator;
+
+public class ternaryoperator {
+	public static void main(String args[]) {
+	 int numa=50, numb=70;
+	 String max;
+	  max = (numa > numb) ? "a is greater " +numb : "b is greater "+numb;
+		 System.out.println(max);
+}
+}
